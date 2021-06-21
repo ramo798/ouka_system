96ab2e14a6d1cfe9d7e35fb17d16c1babb065f35
ramo798 <knht31i@gmail.com>
2021-06-22 02:23:05 +0900

    fix

--- db/mysql_init/1_create.sql
+++ db/mysql_init/1_create.sql
@@ -1,11 +1,11 @@
 use my_database;
 
 CREATE TABLE kaouisan (
-    id INT(10) AUTO_INCREMENT NOT NULL,
-    control_number VARCHAR(20),
+    id INT AUTO_INCREMENT NOT NULL,
+    control_number VARCHAR(50),
     era_name VARCHAR(10),
-    japanese_calendar VARCHAR(16),
-    western_calendar int(12),
+    japanese_calendar VARCHAR(50),
+    western_calendar VARCHAR(50),
     calendar datetime,
     document_name VARCHAR(50),
     persons_name VARCHAR(50),
@@ -15,3 +15,8 @@ CREATE TABLE kaouisan (
     pic_path VARCHAR(255),
     PRIMARY KEY (id)
 );
+
+
+-- INSERT INTO kaouisan (control_number,era_name,japanese_calendar,western_calendar,document_name,persons_name,historical_materials,position_name,pic_no,pic_path) VALUE ('00000002','建武','建武３年３月２０日',13360030200,'藤原数門軍忠状証判','上野頼兼','武雄神社文書','承了（花押）','00000002.bmp','kaou\kaouis\kaoi1\0603082\00000002.bmp');
+-- INSERT INTO kaouisan (control_number,era_name,japanese_calendar,western_calendar,document_name,persons_name,historical_materials,position_name,pic_no,pic_path) VALUE ('00000003','建武２','建武３23年３月２０日',13360030202,'藤原数門軍忠状証判1111','上野頼兼33','2121武雄神社文書','承了（2121212花押）','00000022.bmp','kaou\kaouis\kaoi1\0603082\00000002.bmp');
+-- INSERT INTO kaouisan (control_number,era_name,japanese_calendar,western_calendar,document_name,persons_name,historical_materials,position_name,pic_no,pic_path) VALUE ('00000004','建武３','建武３2323年３月２０日',13360030202,'藤原数門軍忠状証判2222','上野22頼兼','武雄1212神社文書','承212了（花押）','00000012.bmp','kaou\kaouis\kaoi1\0603082\00000002.bmp');
